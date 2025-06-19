50aeab8d5b1c7d3c4d3985e4040941842bfbbea9
santiskrr <purpleblacksouls@gmail.com>
2025-06-19 20:45:40 -0300

    devs

--- src/pages/Devs.jsx
+++ src/pages/Devs.jsx
@@ -4,7 +4,7 @@ export default function Devs() {
   return (
     <>
     <div className="bg-black">
-       <div className="flex justify-center gap-10"> 
+       <div className="flex justify-center gap-10 m-15"> 
 <a href="https://github.com/santiskrr" target="_blank" rel="noopener noreferrer">
         
         <ProfileCard
@@ -16,7 +16,6 @@ export default function Devs() {
         avatarUrl="https://media.istockphoto.com/id/1142192548/es/vector/perfil-de-avatar-hombre-silueta-de-cara-masculina-o-icono-aislado-sobre-fondo-blanco.jpg?s=612x612&w=0&k=20&c=O6KtgzjlrIvoGi2Cb1ZyppWKlqGL_5IXVHLUdLN33Ag="
         showUserInfo={true}
         enableTilt={true}
-        onContactClick={() => console.log('Contact clicked')}
         />
         </a>
 
@@ -32,7 +31,6 @@ export default function Devs() {
         avatarUrl="https://media.istockphoto.com/id/1142192548/es/vector/perfil-de-avatar-hombre-silueta-de-cara-masculina-o-icono-aislado-sobre-fondo-blanco.jpg?s=612x612&w=0&k=20&c=O6KtgzjlrIvoGi2Cb1ZyppWKlqGL_5IXVHLUdLN33Ag="
         showUserInfo={true}
         enableTilt={true}
-        onContactClick={() => console.log('Contact clicked')}
         />
 </a>
         </div>
